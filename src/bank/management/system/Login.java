11067d68169d1f7e1b524bb95838378c333aba4b
AradhanaMote <aradhana2434@gmail.com>
2026-01-10 12:52:40 +0530

    4th commit,Transactions added.

--- src/bank/management/system/Login.java
+++ src/bank/management/system/Login.java
@@ -7,11 +7,8 @@ package bank.management.system;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
 
-/**
- *
- * @author aradh
- */
 public class Login extends JFrame implements ActionListener{
     
     JButton login, signup, clear;
@@ -89,6 +86,21 @@ public class Login extends JFrame implements ActionListener{
             cardTextField.setText("");
             pinTextField.setText("");
         } else if (ae.getSource() == login) {
+            Conn conn = new Conn();
+            String cardnumber = cardTextField.getText();
+            String pin = pinTextField.getText();
+            String query = "select * from login where cardnumber = '"+cardnumber+"' and pin = '"+pin+"'";
+            try {
+                ResultSet rs = conn.s.executeQuery(query);
+                if (rs.next()) {
+                    setVisible(false);
+                    new Transactions(pin).setVisible(true);
+                }else {
+                    JOptionPane.showMessageDialog(null, "Incorrect Card Number or Pin");
+                }
+            }catch (Exception e) {
+                System.out.println(e);
+            }
             
         } else if (ae.getSource() == signup) {
             setVisible(false);
